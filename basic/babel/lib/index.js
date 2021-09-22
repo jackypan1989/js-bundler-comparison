cc697ca0f622cd9394a7170aa1972a235707bc88
jackypan1989 <jackypan1989@gmail.com>
2021-09-22 14:52:16 +0800

    feat: add basic/babel

---
+++ basic/babel/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var example = 'example';
+var _default = example;
+exports["default"] = _default;
